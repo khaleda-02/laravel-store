ce172c565d0d345e64812651659eb207f5e195c9
khaleda-02 <khaleda.02f@gmail.com>
2023-09-25 11:55:58 +0300

    sidebar enhancment

---
+++ config/sidebar.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  [
+    'title' => 'dashboard',
+    'icon' => 'far fa-circle nav-icon',
+    'route' => 'dashboard.index',
+    'active' => ''
+  ],
+  [
+    'title' => 'categories',
+    'icon' => 'nav-icon fas fa-th',
+    'route' => 'dashboard.categories.index',
+    'active' => ''
+  ],
+];
